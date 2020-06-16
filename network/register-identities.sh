2d367b0f6b442a38336636d5d9339720e478c9c1
awjh-ibm <andrew.hurt1@ibm.com>
2020-06-16 13:58:02 +0000

    add identities
    Signed-off-by: awjh-ibm <andrew.hurt1@ibm.com>

--- network/register-identities.sh
+++ network/register-identities.sh
@@ -5,11 +5,27 @@ set -e
 
 cd "$(dirname "$0")"
 
-set -x
+# set -x
 
-mkdir output/Arium
-mkdir output/PrinceInsurance
-mkdir output/VDA
+mkdir -p "output/Arium/Connection Profiles"
+mkdir -p "output/Arium/Identities"
+mkdir -p "output/PrinceInsurance/Connection Profiles"
+mkdir -p "output/PrinceInsurance/Identities"
+mkdir -p "output/VDA/Connection Profiles"
+mkdir -p "output/VDA/Identities"
 
-# TODO - MAKE IT HANDLE ATTRIBUTES
-ansible-playbook identity/register-arium-identity.yml --extra-vars "enrollment_id=registrar enrollment_secret=registrarpw enrollment_type=client"
+register_identities() {
+    for i in $(jq -c '.[]' ../apps/${1}/config/users.json); do
+        NAME=$(jq -r '.name' <<< "$i")
+        ATTRIBUTES=$(jq -c '.attrs' <<< "$i")
+
+        ansible-playbook identity/register-${1}-identity.yml --extra-var="{\"enrollment_id\": \"$NAME\", \"enrollment_secret\": \"${NAME}pw\", \"enrollment_type\": \"client\", \"attributes\": $ATTRIBUTES}"
+        ansible-playbook identity/enroll-${1}-identity.yml --extra-var="{\"enrollment_id\": \"$NAME\", \"enrollment_secret\": \"${NAME}pw\"}"
+    done
+
+    ansible-playbook identity/create-${1}-connection-profile.yml
+}
+
+register_identities manufacturer
+register_identities insurer
+register_identities regulator
