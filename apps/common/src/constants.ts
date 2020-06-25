735dd8ba47e9cfbf67dbf9eb8bfd07f7877b324d
Andrew Hurt <andrew.hurt1@ibm.com>
2020-06-25 16:44:08 +0100

    rename contract
    Signed-off-by: Andrew Hurt <andrew.hurt1@ibm.com>

--- apps/common/src/constants.ts
+++ apps/common/src/constants.ts
@@ -11,7 +11,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-export const CHAINCODE_NAME = 'vehicle-manufacture-chaincode';
+export const CHAINCODE_NAME = 'vehicle-manufacture-contract';
 export const CHANNEL_NAME = 'vehiclemanufacture';
 
 export const DEFAULT_LOCAL_FABRIC_PATH = './vehiclemanufacture_fabric';
